50520bb06bb3f23f70b2969f2056bf68e263d122
SeminCha <cing9393@gmail.com>
2016-10-05 01:06:46 +0900

    Tmap 인증코드 주석 제거

--- app/src/main/java/ensharp/ttalawa/MainActivity.java
+++ app/src/main/java/ensharp/ttalawa/MainActivity.java
@@ -181,7 +181,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mGoogleMap.getUiSettings().setMyLocationButtonEnabled(false);
 
         mapButtonSetting();
-        //TmapAuthentication();
+        TmapAuthentication();
 
         markerMap = new HashMap();
         stationMarkerMap = new HashMap();
